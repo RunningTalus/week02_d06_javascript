4c251a70050e443a4c2dfc9fcef5c082af176655
runningtalus <dave@davemay.org>
2014-04-07 10:13:35 -0600

    Added README and lecture notes

---
+++ README.md
@@ -0,0 +1,9 @@
+README.md 
+
+Introduction to JavaScript
+
+1) Keep console open! Look for line #.
+2) Use console.log
+3) Indent properly
+	-After opening curly brace({), indent one level.
+	-Closing curly brace (}) on its own line at same indentation level as the opening curly brace ({).
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+// If it's sunny out, I'm going to ride my bike to work.
+// If not, I'll drive.
+// But, if my girlfriend is using the car for the day, I'll take the bus.
+
 var sunnyOut = false;
 var carTaken = true;
+
 if (sunnyOut) {
 	alert("I\'ll ride my bike!");
 }
@@ -7,5 +12,5 @@ else if(carTaken) {
 	alert("I\'ll take the bus!");
 }
 else {
-	alert("I\'m driving to work!");
+	alert("I\'m drive to work!");
 }
