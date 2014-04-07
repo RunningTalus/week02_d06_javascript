3c1369749fcf13ea42e99f683c0f3b49633118bb
runningtalus <dave@davemay.org>
2014-04-07 10:03:34 -0600

    First commit - introduction to JavaScript

---
+++ main.js
@@ -0,0 +1,11 @@
+var sunnyOut = false;
+var carTaken = true;
+if (sunnyOut) {
+	alert("I\'ll ride my bike!");
+}
+else if(carTaken) {
+	alert("I\'ll take the bus!");
+}
+else {
+	alert("I\'m driving to work!");
+}
